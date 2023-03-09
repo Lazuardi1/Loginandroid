ba2834daf54fd0347e25a1f8a181c1ac0e328759
Lazuardi1 <lazuardisulaiman04@gmail.com>
2023-03-09 12:19:51 +0700

    awal

---
+++ Login/app/src/main/java/com/ukom/login/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.ukom.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void registActivity(View view) {
+        Intent I=new Intent(LoginActivity.this,RegisterActivity.class);
+        startActivity(I);
+    }
+//forgot password
+//    public void ForgotPasswordActivity(View view) {
+//        Intent I=new Intent(LoginActivity.this, ForgotPasswordActivity.class);
+//                startActivity(I);
+//    }
+}
